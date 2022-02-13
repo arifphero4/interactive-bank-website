36b8245a7c4a6cede703832a779a124382fd8dde
arifphero <ahammadarif232@gmail.com>
2022-02-14 05:35:17 +0600

    balance section added

--- js/bank.js
+++ js/bank.js
@@ -1,8 +1,13 @@
 document.getElementById("login-button").addEventListener("click", function () {
+  // user email
   const emailField = document.getElementById("user-email");
   const userEmail = emailField.value;
+
+  //   user password
   const passwordField = document.getElementById("user-password");
   const userPassword = passwordField.value;
+
+  //   check user email & password
   if (userEmail == "admin@bank.com" && userPassword == 12345) {
     window.location.href = "banking.html";
     emailField.value = "";
