eba831575538dcb122367bb160a377216dee26e1
arifphero <ahammadarif232@gmail.com>
2022-02-14 08:00:46 +0600

    updated balance total after withdraw

--- js/banking.js
+++ js/banking.js
@@ -5,8 +5,8 @@ document
     //get deposite amount
     const depositInput = document.getElementById("deposit-input");
 
-    const newDepositAmoutText = depositInput.value;
-    const newDepositAmout = parseFloat(newDepositAmoutText);
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmout = parseFloat(newDepositAmountText);
 
     // update deposit total
     const depositTotal = document.getElementById("deposit-total");
@@ -29,3 +29,36 @@ document
     //clear input field
     depositInput.value = "";
   });
+
+// handle withdraw button event
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    //get withdraw amount
+    const withdrawInput = document.getElementById("withdraw-input");
+    const newWithdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountText);
+
+    //update withdraw amount
+    const withdrawTotal = document.getElementById("withdraw-total");
+
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawAmount = parseFloat(previousWithdrawText);
+
+    const newWithdrawtotal = previousWithdrawAmount + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawtotal;
+
+    // update account balance
+    const balanceTotal = document.getElementById("balance-total");
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear input field
+    withdrawInput.value = "";
+  });
+
+// update account balance
